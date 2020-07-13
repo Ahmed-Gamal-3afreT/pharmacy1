71052b67c205e557f2aeaf963bd47b842d46080d
Ahmed 3afreT <Mr-@live.com>
2020-07-13 22:46:02 +0200

    first commit

---
+++ assets/constants/config.php
@@ -0,0 +1,19 @@
+<?php
+//database settings
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "pharmacy1";
+
+$currency = "INR";
+/*date_default_timezone_set('Africa/Dar_es_salaam');*/
+
+date_default_timezone_set('Africa/Cairo');
+
+//Installation - Include the last slash
+$instalation_dir = "http://localhost/pharmacy/";
+
+//Mail settings
+$smtp_host = 'smtp.mailtrap.io';
+$smtp_user = 'bc20c7a3c10752';
+$smtp_pass = '5e2a2f540612fd';
